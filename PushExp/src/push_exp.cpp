a10e52f8b408ce3ca1b78d74e078fb53ab4e43cf
robinzhoucmu <jiajiz@andrew.cmu.edu>
2015-07-24 18:30:48 -0400

    minor change

--- PushExp/src/push_exp.cpp
+++ PushExp/src/push_exp.cpp
@@ -1,4 +1,8 @@
-# include "push_exp.h"
+#include "push_exp.h"
+
+// Global flag indicating whether the robot is pushing the object.
+bool flag_is_pushing;
+std::vector<geometry_msgs::Pose> 
 
 int main(int argc, char* argv[]) {
   return 0;
--- PushExp/src/push_exp.h
+++ PushExp/src/push_exp.h
@@ -6,12 +6,12 @@
 #define PUSH_EXP_H
 
 #include <iostream>
+#include <vector>
 // ROS related. 
 #include <ros/ros.h>
 #include <robot_comm/robot_comm.h>
 #include <Mocap/mocap_comm.h>
 #include <matVec/matVec.h>
-
 
 class PushExp {
  public:
@@ -21,20 +21,27 @@ class PushExp {
 
  private:
   RobotComm* robot;
+  ros::AsyncSpinner asyn_spinner;
+
   // Mocap subscription.
   ros::Subscriber mocap_sub;
   // Force subscription.
   ros::Subscriber force_sub;
   int num_pushes;
   // Command the robot to a resting position that won't block the mocap nor touch the object.
-  void GotoRobotRestState();
+  void GotoRobotRestingState();
   // Let the mocap acquire STATIC object poses assuming the robot is not blocking view.
   void AcquireObjectStablePose();
   
+  // 1) Move from the robot from rest state to pre-approach state.
+  // 2) Approach object and push.
+  // 3) Leave contact. 
   void ExecuteSingleRobotPush();
-  // Log the pushing force while robot is pushing/in contact with the object. 
+  // Log the pushing force while robot is pushing/in contact with the object.
+  // Uses async spinner for logging. Call this function before calling robotSetCartesian.
+  // Remember to stop the Async spinner after robotSetCartesian.
   void LogPushForce();
-  // 
+  
   
   
 };
