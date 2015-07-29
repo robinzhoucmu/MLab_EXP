9fad7f63a9fac647801bf25a8e6d855848c815fb
robinzhoucmu <jiajiz@andrew.cmu.edu>
2015-07-29 14:15:39 -0400

    weird

--- PushExp/src/PushGenerator.h
+++ PushExp/src/PushGenerator.h
@@ -27,11 +27,11 @@ class PushGenerator
     double MIN_PUSH_ANGLE = 20.0;
 
     // The distance away from the push point to start the object (mm)
-    const static double DEFAULT_INITIAL_DISTANCE = 50.0;
+    double DEFAULT_INITIAL_DISTANCE = 50.0;
     // The distance past the push point to push the object (mm)
-    const static double DEFAULT_PENETRATION_DISTANCE = 20.0;
+    double DEFAULT_PENETRATION_DISTANCE = 20.0;
     // The distance to retract after pushing (mm)
-    const static double DEFAULT_RETRACTION_DISTANCE = 5.0;
+    double DEFAULT_RETRACTION_DISTANCE = 5.0;
 
     PushGenerator() { 
       srand(time(NULL)); 
