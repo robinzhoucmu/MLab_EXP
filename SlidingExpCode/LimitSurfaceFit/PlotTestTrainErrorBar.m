4a2680d8c1b439471ec93b366133bb1cc82e50dd
robinzhoucmu <jiajiz@cs.cmu.edu>
2015-08-14 23:41:36 -0400

    add lots of figures

--- SlidingExpCode/LimitSurfaceFit/PlotTestTrainErrorBar.m
+++ SlidingExpCode/LimitSurfaceFit/PlotTestTrainErrorBar.m
@@ -3,26 +3,26 @@ num_methods = 4;
 %plot graph.
 colors = ['r','g','b','k'];
 h1 = figure;
-title('testing error');
+%title('testing error');
 hold on;
 
 
 
 r = [0.15, 0.3, 0.5, 1.0];
 for i = 1:1:num_methods
-    [mu, sigma, muci, sigmaci] = normfit(exp_record.err_test{i}, 0.05);
+    [mu, sigma, muci, sigmaci] = normfit(exp_record.err_test{i}, 0.05)
     p = errorbar(r, mu, muci(2,:)-mu, ...
-        'Color', colors(i), 'Marker', '*', 'MarkerSize', 6);
+        'Color', colors(i), 'Marker', 'o', 'MarkerSize', 6, 'LineStyle','--');
 end
 legend('poly4-cvx','poly4-plain','quadratic', 'gp');
 
 h2 = figure;
-title('training error');
+%title('training error');
 hold on;
 for i = 1:1:num_methods
     [mu, sigma, muci, sigmaci] = normfit(exp_record.err_train{i}, 0.05);
     p = errorbar(r, mu, muci(2,:)-mu, ...
-        'Color', colors(i), 'Marker', '*', 'MarkerSize', 6);
+        'Color', colors(i), 'Marker', 'o', 'MarkerSize', 6, 'LineStyle','--');
 end
 legend('poly4-cvx','poly4-plain','quadratic', 'gp');
 
--- SlidingExpCode/LimitSurfaceFit/demo_simulation_comparison.m
+++ SlidingExpCode/LimitSurfaceFit/demo_simulation_comparison.m
@@ -5,28 +5,20 @@ exp_para.num_evals = 50;
 exp_para.num_data = 150;
 exp_para.r_facet = 0.5;
 exp_para.r_train = 0.5;
-exp_para.noise.f = 0;
-exp_para.noise.v = 0;
+exp_para.noise.f = 0.1;
+exp_para.noise.v = 0.1;
 
-% Two bar supports. 
-% options_pt.mode = 'rim';
-% options_pt.range = 1;
-% num_pts = 2;
-% options_pd.mode = 'random';
+% scenario = '2-bar';
+% [exp_record_bar] = RunSimulationScenario(scenario, exp_para);
+% toc;
+% [h1_bar,h2_bar] = PlotTestTrainErrorBar(exp_record_bar);
 % 
-% [exp_record_bar] = MultiEval2(num_evals, num_pts, num_data, options_pt, ...
-%                               options_pd, r_facet, r_train, noise)
-scenario = '2-bar';
-[exp_record_bar] = RunSimulationScenario(scenario, exp_para);
-toc;
-[h1_bar,h2_bar] = PlotTestTrainErrorBar(exp_record_bar);
-
-scenario = '3-points';
-[exp_record_3pts] = RunSimulationScenario(scenario, exp_para);
-toc;
-[h1_3pts,h2_3pts] = PlotTestTrainErrorBar(exp_record_3pts);
-
-save('ExpRecord/50_evals_150_data_0f_0v', 'exp_record_bar', 'exp_record_3pts');
+% scenario = '3-points';
+% [exp_record_3pts] = RunSimulationScenario(scenario, exp_para);
+% toc;
+% [h1_3pts,h2_3pts] = PlotTestTrainErrorBar(exp_record_3pts);
+% 
+% save('ExpRecord/50_evals_150_data_0f_0v', 'exp_record_bar', 'exp_record_3pts');
 
 
 scenario = '360-ring';
