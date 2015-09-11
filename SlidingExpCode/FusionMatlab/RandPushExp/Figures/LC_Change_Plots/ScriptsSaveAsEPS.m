110a9a0321cd6f6f8c0bcc7859f4f1afc91626f9
robinzhoucmu <jiajiz@andrew.cmu.edu>
2015-09-11 17:37:54 -0400

    figre plot change

--- SlidingExpCode/FusionMatlab/RandPushExp/Figures/LC_Change_Plots/ScriptsSaveAsEPS.m
+++ SlidingExpCode/FusionMatlab/RandPushExp/Figures/LC_Change_Plots/ScriptsSaveAsEPS.m
@@ -1,11 +1,18 @@
 close all; 
 % Extract all the fig files.
-k = dir('*.fig');
+k = dir('InPaper/*.fig');
 filenames = {k.name}';
 for i =1:1:length(filenames)
     file_name = filenames{i}(1:end-4);
     h = openfig(file_name);
-    saveas(h, [file_name, '.eps'], 'epsc');
+    %xlabel('F_x', 'FontSize', 10);ylabel('F_y', 'FontSize', 10);zlabel('F_z', 'FontSize', 10);
+    %et(findall(gcf,'-property','FontName'),'FontName','Times New Roman');
+    %set(findall(gcf,'-property','FontSize'),'FontSize', 10);
+    pause;
+    export_fig(gcf, file_name, '-eps');
+    %saveas(h, [file_name, '.eps'], 'eps');
+    %print(gcf, '-depsc', '-painters', [file_name, '.eps']);
+    close all;
 end
 
 % file_name_fig = '50_10_ideal'; 
--- SlidingExpCode/FusionMatlab/RandPushExp/ScriptAverageBarPlot.m
+++ SlidingExpCode/FusionMatlab/RandPushExp/ScriptAverageBarPlot.m
@@ -1,5 +1,6 @@
+close all;
 % Scripts to generate average plots.
-%load 15_evals_exp_sim_compare.mat;
+
 load blk_15_evals_exp_sim_compare.mat;
 num_logs = length(exp_record_set);
 num_methods = 4;
@@ -14,10 +15,12 @@ for ind_method = 1:1:4
     end
     err_test_all_exp{ind_method} = data;
 end
- group_name = {'5', '10', '20', '30'};
- h_exp = PlotBarsWithErrors(err_test_all_exp, group_name, 1);
+group_name = {'5', '10', '20', '30'};
+h_exp = PlotBarsWithErrors(err_test_all_exp, group_name, 1);
 ImproveFigure(h_exp);
- 
+file_name_fig = '/home/jiaji/Dropbox/icra_conf/icra_conference_submission/Figures/RobotExpBarPlots/exp_avg_blkboard2.eps';
+print(h_exp, file_name_fig, '-deps');
+
  % Sim test performance bar.
 err_test_all_sim = {};
 for ind_method = 1:1:4
@@ -27,8 +30,45 @@ for ind_method = 1:1:4
     end
     err_test_all_sim{ind_method} = data;
 end
- group_name = {'5', '10', '20', '30'};
- h_sim = PlotBarsWithErrors(err_test_all_sim, group_name, 1);
- ImproveFigure(h_sim);
- 
- 
+group_name = {'5', '10', '20', '30'};
+h_sim = PlotBarsWithErrors(err_test_all_sim, group_name, 1);
+ImproveFigure(h_sim);
+file_name_fig = '/home/jiaji/Dropbox/icra_conf/icra_conference_submission/Figures/RobotExpBarPlots/sim_avg_blkboard2.eps';
+print(h_sim, file_name_fig, '-deps');
+
+clear all;
+close all;
+load 15_evals_exp_sim_compare.mat;
+num_logs = length(exp_record_set);
+num_methods = 4;
+num_train_splits = 4;
+
+% Exp test performance bar.
+err_test_all_exp = {};
+for ind_method = 1:1:4
+    data = zeros(num_evals * num_logs, num_train_splits);
+    for i = 1:1:num_logs
+        data((i-1)*num_evals+1:i*num_evals, :) = exp_record_set{i}.err_test{ind_method};
+    end
+    err_test_all_exp{ind_method} = data;
+end
+group_name = {'5', '10', '20', '30'};
+h_exp = PlotBarsWithErrors(err_test_all_exp, group_name, 1);
+ImproveFigure(h_exp);
+file_name_fig = '/home/jiaji/Dropbox/icra_conf/icra_conference_submission/Figures/RobotExpBarPlots/exp_avg_wood2.eps';
+print(h_exp, file_name_fig, '-deps');
+
+ % Sim test performance bar.
+err_test_all_sim = {};
+for ind_method = 1:1:4
+    data = zeros(num_evals * num_logs, num_train_splits);
+    for i = 1:1:num_logs
+        data((i-1)*num_evals+1:i*num_evals, :) = sim_record_set{i}.err_test{ind_method};
+    end
+    err_test_all_sim{ind_method} = data;
+end
+group_name = {'5', '10', '20', '30'};
+h_sim = PlotBarsWithErrors(err_test_all_sim, group_name, 1);
+ImproveFigure(h_sim);
+file_name_fig = '/home/jiaji/Dropbox/icra_conf/icra_conference_submission/Figures/RobotExpBarPlots/sim_avg_wood2.eps';
+print(h_sim, file_name_fig, '-deps');
